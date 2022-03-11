4d59da476b3f85ed131fb48310b41dfeea9f8ce9
MG <mg-git.plumbic@8shield.net>
2022-03-11 15:00:53 +0800

    Create SplashScreen

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,18 +1,38 @@
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class GameWindow {
 
+    //Screen Dimension
+    int gameWidth = 1280;
+    int gameHeight = 720;
+
+    //JFrames
     JFrame frame = new JFrame("Moon Lander");
+    JFrame splashFrame;
+
+    //JPanels
+    JPanel splashPanel;
 
     GameWindow(){
+
+    }
+
+    public void createAndShowCanvas() {
         frame.setVisible(true);
-        frame.setBounds(350,40,700,600);
+        frame.setBounds(350,40, gameWidth, gameHeight);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         GameCanvas canvas = new GameCanvas();
         frame.add(canvas);
+    }
+
+    public void createAndShowSplashScreen () {
+        splashFrame = new JFrame("Splash Screen");
+        splashPanel = new JPanel();
+
+
     }
 
 }
--- src/MainGame.java
+++ src/MainGame.java
@@ -3,6 +3,7 @@ public class MainGame {
 
     public static void main(String[] args) {
         GameWindow window = new GameWindow();
+        window.createAndShowCanvas();
     }
 
 }

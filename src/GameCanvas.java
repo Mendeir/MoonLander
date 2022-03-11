d94d8390204a8aa347e3a2f1b5318da00954427f
dbornilla <78960805+dbornilla@users.noreply.github.com>
2022-03-11 14:50:35 +0800

    Canvas View

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -5,21 +5,34 @@ import java.util.*;
 import javax.swing.*;
 
 public class GameCanvas extends Canvas implements KeyListener, ActionListener {
+
     Rocket rocket;
 
     GameCanvas(){
 
         rocket = new Rocket(10,50,500);
+        addKeyListener(this);
         setBackground(Color.black);
         setFocusable(true);
 
     }
 
+    public void paint(Graphics g){
 
-    public void paint(Graphics2D g){
-        g.draw(rocket.getShape());
+        Graphics2D gd = (Graphics2D)g;
+        super.paint(g);
+        BufferedImage gameView = bufferedGame();
+        gd.drawImage(gameView,0,0,null);
     }
 
+    public BufferedImage bufferedGame(){
+        BufferedImage b = new BufferedImage(this.getWidth(),this.getHeight(),BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = b.createGraphics();
+        g.setPaint(Color.white);
+        g.draw(rocket.getShape());
+
+        return b;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -34,13 +47,13 @@ public class GameCanvas extends Canvas implements KeyListener, ActionListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_UP){
-
+            System.out.println("up");
         }
         if(e.getKeyCode() == KeyEvent.VK_LEFT){
-
+            System.out.println("left");
         }
         if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-
+            System.out.println("right");
         }
     }
 
